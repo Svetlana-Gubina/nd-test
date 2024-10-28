0cca2960529c0ba61ff97f5ce2e0dff37f0ce310
Svetlana-Gubina <gubinas83@gmail.com>
2024-10-28 10:19:04 +0300

    reset router 2

--- src/router/router.js
+++ src/router/router.js
@@ -22,7 +22,7 @@ const router = createRouter({
       component: () => import("@/components/PageNotFoundView.vue"),
     },
   ],
-  history: createWebHashHistory("/"),
+  history: createWebHistory("/"),
 });
 
 router.beforeEach((to, from, next) => {
